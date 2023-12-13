442c255701cc318b000fb1eb746d1496077cb3bc
Steph <108551327+s-hens@users.noreply.github.com>
2023-12-13 18:30:27 +0000

    Component to edit name on certificate

--- src/cert-text.jsx
+++ src/cert-text.jsx
@@ -3,30 +3,45 @@ import { useState } from "react";
 function CertText() {
 
     const [petName, setPetName] = useState("Pooky");
+    const [editing, setEditing] = useState(0);
 
-    function Button() {
-        function handleClick() {
-            console.log("Hello");
+    function EditButton({ editing }) {
+        function handleEdit(e) {
+            if (!editing) {
+                setEditing(1);
+            } else {
+                setEditing(0);
+                setPetName(e.target.value);
+            }
         }
-
-        return (
-            <button onClick={handleClick}>
-              Edit
-            </button>
-          );
+        if (!editing) {
+            return (
+                <>
+                {petName}
+                <button onClick={handleEdit}>
+                    Edit
+                </button>
+                </>
+            );
+        } else {
+            return (
+                <>
+                <input type="text" id="pet-name" defaultValue={petName} onBlur={handleEdit}/>
+                <button onClick={handleEdit}>
+                    Save
+                </button>
+                </>
+            )
+        }
     }
 
     return (
         <>
         <div id="cert">
             <p>CertifiCATe of Achievement</p>
-            <p>
-                <span id="name">
-                    Awarded to {petName} 
-                </span>
-                <Button />
-                <input type="text" id="pet-name" value={petName} onChange={(event) => setPetName(event.target.value)}/>
-            </p>
+            <p>This certifies that</p>
+            <p><EditButton editing={editing}/></p>
+            <p>is a good boy/girl!</p>
         </div>
         </>
     )
